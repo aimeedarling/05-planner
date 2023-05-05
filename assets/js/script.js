3fdbd393f60b940aea55b8aa97e8d98d7884e3bd
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-05-04 19:22:51 -0700

    fixed secons not refreshing

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,15 +23,17 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    let now = dayjs()
+  
+    $('#currentDay').text(dayjs().format('dddd, MMMM, D, YYYY, HH:mm:ss'))
     
+
     // TODO: Add a listener for click events on the save button. This code should
-    $('.saveBtn').click(function(event){
+    $('.saveBtn').click(function (event) {
         let target = $(event.target)
         let storedContent = target.siblings('textarea').val()
         localStorage.setItem(target.parent().attr('id'), storedContent);
     });
- 
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -39,19 +41,20 @@ $(function () {
     // current hour in 24-hour time?
     //
 
-    
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-    
 
     // TODO: Add code to display the current date in the header of the page.
-    $('#currentDay').val(now)
-    
+    function displayDateTime() {
+        $('#currentDay').text(dayjs().format('dddd, MMMM, D, YYYY, HH:mm:ss'))
 
+    }
+
+    setInterval(displayDateTime, 1000)
 
 });
-        
 
-    
+
+
